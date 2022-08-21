2bf5ac2f9a28a2a73a46a84f5c507119a46a3f7d
maculewicz <maculewicz.au@gmail.com>
2022-08-21 15:57:29 +1000

    update

---
+++ break-continue.js
@@ -0,0 +1,16 @@
+//break and continue
+//stworzmy petle ktora bedzie licznikiem
+for(let i = 0; i<10; i++){
+    //break
+    //if(i === 4){
+    //    console.log("jest rowne")
+    //   break;   //jesli dodamy break nasza petla jest calkowicie wstrzymywana i nie bedzie juz wiecej wykonywana, break calkowicie ta nasza petle zatrzymuj0
+    //   }
+    // console.log(i)
+
+//continue
+if(i === 4 || i == 6){
+    console.log("jest rowne")
+}
+console.log(i)
+}
