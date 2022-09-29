80214e6706f0430469c5df9b3442826ace2389c5
Robert Maculewicz <robertmaculewicz@Roberts-MBP.modem>
2022-09-29 20:40:38 +1000

    last

--- instrukcje.js
+++ instrukcje.js
@@ -3,6 +3,7 @@ const plec = "robot"; //tu moze byc: menszczyzna, kobieta, robot
 const wiek = 35;
 const zwierzeta = "kot"
 
+
 // if(plec == "kobieta") {
 //     console.log("Hej wiew, ze jestes kobieta")
 // } else if(plec == "robot"){
